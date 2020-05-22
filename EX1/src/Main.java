770af3887c18283a4d315ddea1737fc9aa322d53
MH <779605603@qq.com>
2020-05-22 11:21:27 +0800

    EX1,指数计算问题（求兔子数目）

---
+++ EX1/src/Main.java
@@ -0,0 +1,31 @@
+/*
+EX1:
+有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子对数为多少？
+
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子对数为多少？");
+        System.out.println("请输入月数：");
+        if(scan.hasNextInt()){
+            n = scan.nextInt();
+            int num = fun(n);
+            System.out.println("第"+n+"个月的兔子总数为："+num);
+        }else{
+            System.out.println("请输入正确月数！");
+        }
+    }
+
+    static int fun(int n){
+        if(n==1||n==2){
+            return 1;
+        }else{
+            return fun(n-1)+fun(n-2);
+        }
+    }
+}
